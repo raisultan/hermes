3a2b7a61857279b7b794589aa06829da7e38e54f
raisultan <ki.xbozz@gmail.com>
2024-01-07 19:11:22 +0600

    read collection name from storage for web

--- hermes/web/main.py
+++ hermes/web/main.py
@@ -1,10 +1,13 @@
+from sqlite3 import Connection
+
 from fastapi import FastAPI, Depends
 from pymilvus import Collection
 
-from hermes.collection import INDEX_CONFIG
+from hermes.collection import INDEX_CONFIG, schema
 from hermes.embed import get_embedding
 from hermes.search import SearchResult, search
-from hermes.web.setup import get_collection
+from hermes.storage import read_collection_name
+from hermes.web.setup import get_milvus_connection, get_db_connection
 from hermes.web.schemas import SearchRequest
 from hermes.normalize import normalize_pdf
 
@@ -14,8 +17,15 @@ app = FastAPI()
 @app.post('/api/search')
 def search_handler(
     req: SearchRequest,
-    collection: Collection = Depends(get_collection),
+    collection: Collection = Depends(get_milvus_connection),
+    db_conn: Connection = Depends(get_db_connection),
 ) -> list[SearchResult]:
+    collection_name = read_collection_name(db_conn)
+    print(f'\n\ncollection name: {collection_name}\n\n')
+    collection = Collection(
+        name=collection_name,
+        schema=schema,
+    )
     normalized = normalize_pdf(req.text)
     embedding = get_embedding(normalized)
     results = search(collection, INDEX_CONFIG, embedding)
--- hermes/web/setup.py
+++ hermes/web/setup.py
@@ -1,15 +1,15 @@
 from pymilvus import Collection
 
-from hermes.collection import connect_milvus, disconnect_milvus, collection_name, schema
+from hermes.collection import connect_milvus, disconnect_milvus
+from hermes.storage import connect_to_db, disconnect_from_db
 
-def get_collection() -> Collection:
+def get_milvus_connection() -> Collection:
     connect_milvus()
-    collection = Collection(
-        name=collection_name,
-        schema=schema,
-    )
+    yield
+    disconnect_milvus()
 
-    try:
-        yield collection
-    finally:
-        disconnect_milvus()
+
+def get_db_connection():
+    conn = connect_to_db()
+    yield conn
+    disconnect_from_db(conn)
